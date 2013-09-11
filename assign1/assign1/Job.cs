462397ad16076aaa3243e204869c98fca512af44
ogogoleg <o_borisevich@yahoo.com>
2013-09-12 01:17:22 +0300

    Question 1 and 2 are done

---
+++ assign1/assign1/Job.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALGO2
+{
+    namespace Assignment1
+    {
+        struct Job
+        {
+
+            public Job(int id, double weight, double length) : this()
+            {
+                Weight = weight;
+                Length = length;
+                Id = id;
+            }
+
+            public double Weight { get; set; }
+            public double Length { get; set; }
+            public int Id { get; set; }
+
+            public double GetWeighedCompletion(double offset)
+            {
+                return Weight * (offset + Length);
+            }
+
+            public override string ToString()
+            {
+                return String.Format("Job Id: {0}, Weight: {1}, Length: {2}", Id, Weight, Length);
+            }
+
+        }
+    }
+    }
--- assign1/assign1/Program.cs
+++ assign1/assign1/Program.cs
@@ -3,13 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text;
 
-namespace assign1
+
+namespace ALGO2
 {
-    class Program
+    namespace Assignment1
     {
-        static void Main(string[] args)
+        class Program
         {
+            static void Main(string[] args)
+            {
+
+                FileStream fs;
+                try
+                {
+                    fs = File.OpenRead(@".\data\jobs.txt");
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return;
+                }
+                StreamReader sr = new StreamReader(fs);
+                int numberOfJobs = Int32.Parse(sr.ReadLine());
+                Job[] jobs = new Job[numberOfJobs];
+                for (int i = 0; i < numberOfJobs; i++)
+                {
+                    string sJob = sr.ReadLine();
+                    var words = sJob.Split();
+                    jobs[i] = new Job(i, Double.Parse(words[0]), Double.Parse(words[1]));
+                }
+
+                jobs = jobs.OrderByDescending(job => (job.Weight - job.Length)).ThenByDescending(job => job.Weight).ToArray();
+
+                double res = 0, offset = 0;
+
+                foreach (Job job in jobs)
+                {
+                    res += job.GetWeighedCompletion(offset);
+                    offset += job.Length;
+                }
+                Console.WriteLine("MinSumComplTimes(Weight-Length) = " + res.ToString());
+
+                res = 0;
+                offset = 0;
+
+                jobs = jobs.OrderByDescending(job => (job.Weight / job.Length)).ToArray();
+                foreach (Job job in jobs)
+                {
+                    res += job.GetWeighedCompletion(offset);
+                    offset += job.Length;
+                }
+                Console.WriteLine("MinSumComplTimes(Weight/Length) = " + res.ToString());
+
+
+            }
+
         }
     }
 }
