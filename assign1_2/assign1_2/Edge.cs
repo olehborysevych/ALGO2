ec97460a55499b86bdefc05baa43dd195183e5c1
ogogoleg <o_borisevich@yahoo.com>
2013-09-15 04:06:51 +0300

    assign1_2 added

---
+++ assign1_2/assign1_2/Edge.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign1_2
+{
+    class Edge
+    {
+        public Edge(int id, int node1, int node2,int cost)
+            {
+                Node1 = node1;
+                Node2 = node2;
+                Cost = cost;
+                Id = id;
+            }
+
+            public double Node1 { get; set; }
+            public double Node2 { get; set; }
+            public double Cost { get; set; }
+            public int Id { get; set; }
+
+
+            public override string ToString()
+            {
+                return String.Format("Edge Id: {0}, Node1: {1}, Node2: {2},  Cost: {3}", Id, Node1, Node2, Cost);
+            }
+    }
+}
---
+++ assign1_2/assign1_2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nEdges = 0;
+            int nNodes;
+            Edge[] edges;
+            FileStream fs;
+            try
+            {
+                fs = File.OpenRead(@".\data\edges.txt");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            StreamReader sr = new StreamReader(fs);
+            string sTemp = sr.ReadLine();
+            var words = sTemp.Split();
+            nNodes = Int32.Parse(words[0]);
+            nEdges = Int32.Parse(words[1]);
+            edges = new Edge[nEdges];
+            for (int i = 0; i < nEdges; i++)
+            {
+                string sEdge = sr.ReadLine();
+                words = sEdge.Split();
+                edges[i] = new Edge(i, Int32.Parse(words[0]), Int32.Parse(words[1]), Int32.Parse(words[2]));
+            }
+
+            edges = edges.OrderBy(edge => edge.Cost).ToArray();
+            foreach (var edge in edges)
+            { 
+            
+            }
+
+        }
+    }
+}
