5a5a413f782eee03fc61e338c139ac402cc75005
ogogoleg <o_borisevich@yahoo.com>
2013-10-26 17:56:11 +0300

    assign4 added

---
+++ assign4/assign4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs;
+            try
+            {
+                fs = File.OpenRead(@".\data\g1.txt");
+                //fs = File.OpenRead(@".\data\test6.txt");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            StreamReader sr = new StreamReader(fs);
+            string sInfo = sr.ReadLine();
+            var parameters = sInfo.Split();
+
+            int nVertices = Int32.Parse(parameters[0]);
+            int nEdges = Int32.Parse(parameters[1]);
+
+            //List<Tuple<float, float>> points = new List<Tuple<float, float>>();
+
+
+            for (int i = 0; i < nEdges; i++)
+            {
+                sInfo = sr.ReadLine();
+                var edge = sInfo.Split();
+                int tail = Int32.Parse(edge[0]);
+                int head = Int32.Parse(edge[1]);
+                //points.Add(new Tuple<float, float>(x, y));
+            }
+
+        }
+    }
+}
